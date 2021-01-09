5ead29354275866b655c3bd79e25c770816c82dc
zi wang <ziwang55@gmail.com>
2021-01-08 23:45:08 -0600

    created jawdb

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,10 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
 CREATE TABLE burgers (
-  id int AUTO_INCREMENT NOT NULL,
-  burger_name varchar(255) NOT NULL,
-  createdAt TIMESTAMP NOT NULL,
-  devoured boolean,
+  id INT AUTO_INCREMENT NOT NULL,
+  burger_name varchar(30) NOT NULL,
+  devoured BOOLEAN,
   PRIMARY KEY(id)
 );
